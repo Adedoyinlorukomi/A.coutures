f58368c53013ee5426d1709695725761b998e6e4
Adedoyinlorukomi <akindoyin800@gmail.com>
2023-08-27 20:34:47 +0100

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,13 @@ module.exports = {
       'xl': {'max': '1279px'},
       // => @media (max-width: 1279px) { ... }
 
-      'lg': {'max': '1023px'},
+      'lg': {'max': '900px'},
       // => @media (max-width: 1023px) { ... }
 
       'md': {'max': '767px'},
       // => @media (max-width: 767px) { ... }
 
-      'sm': {'max': '639px'},
+      'sm': {'max': '428px'},
       // => @media (max-width: 639px) { ... }
     },
     extend: {},
